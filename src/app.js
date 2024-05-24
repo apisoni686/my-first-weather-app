28a58558e09e92477db0053f170f43a41e1bc307
Amy :) <apisoni686@gmail.com>
2024-05-24 15:18:25 -0700

    added basic js functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-alert("Hello!");
+function searchWeather(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-input");
+  let city = document.querySelector("#displayed-city");
+  city.innerHTML = input.value;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchWeather);

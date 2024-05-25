004b00c2dc6534dc9fc26a8a5dc5e8a661fdeff6
Amy :) <apisoni686@gmail.com>
2024-05-24 19:58:39 -0700

    updated displayed city with api data

--- src/app.js
+++ src/app.js
@@ -3,19 +3,20 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#displayed-temp");
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+  let city = document.querySelector("#displayed-city");
+  city.innerHTML = response.data.city;
 }
 
 function searchCity(city) {
   let apiKey = "df01d4d69fab3f5otaf6694bc9e08ea8";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(updateWeather);
 }
 
 function searchWeather(event) {
   event.preventDefault();
   let input = document.querySelector("#search-input");
-  let city = document.querySelector("#displayed-city");
-  city.innerHTML = input.value;
+
   searchCity(input.value);
 }
 

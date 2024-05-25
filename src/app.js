0d9cd0ae6eb0fe891f4a70fc83345149664b861f
Amy :) <apisoni686@gmail.com>
2024-05-24 19:33:21 -0700

    added api integration for displaying current temp

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#displayed-temp");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "df01d4d69fab3f5otaf6694bc9e08ea8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchWeather(event) {
   event.preventDefault();
   let input = document.querySelector("#search-input");
   let city = document.querySelector("#displayed-city");
   city.innerHTML = input.value;
+  searchCity(input.value);
 }
 
 let form = document.querySelector("#search-form");
